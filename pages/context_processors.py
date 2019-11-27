bebaacb9ce75b52eace627beabadfc28de155c1c
rafiyasir <40689559+rafiyasir@users.noreply.github.com>
2019-11-27 14:56:53 +0530

    updated Context

--- pages/context_processors.py
+++ pages/context_processors.py
@@ -3,9 +3,9 @@ from promotionals.models import Promotional
 from miscellaneous.models import Miscellaneous
 
 def products_dropdown(request):
-    drpdwn_bags = Bag.objects.all()
-    drpdwn_promos = Promotional.objects.all()
-    drpdwn_miscs = Miscellaneous.objects.all()
+    drpdwn_bags = Bag.objects.all().filter(is_published=True)
+    drpdwn_promos = Promotional.objects.all().filter(is_published=True)
+    drpdwn_miscs = Miscellaneous.objects.all().filter(is_published=True)
 
     context = {
         'drpdwn_bags':drpdwn_bags,
